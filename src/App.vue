651dbdfeef0516c18291269f94ef5e70866a8d05
shoeiedibistl <shoeiedibistl@mail.ru>
2025-07-25 00:24:49 +0300

    upd

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,40 @@
   <div
     class="w-[1080px] mx-auto my-10 bg-white rounded-4xl min-h-[calc(100vh-5rem)] shadow-xl"
   >
+    <!--
     <Drawer />
+    -->
     <Header />
-    <h1 class="text-xl text-4xl font-bold p-10">Все кроссовки</h1>
+
+    <div class="flex gap-10 items-center justify-between px-10 mt-10">
+      <h1 class="text-xl text-4xl font-bold">Все кроссовки</h1>
+
+      <div class="flex items-center gap-5">
+        <select
+          name=""
+          id=""
+          class="px-5 py-2 border border-slate-300 rounded-xl outline-none cursor-pointer"
+        >
+          <option value="">По названию</option>
+          <option value="">По цене (дешевле)</option>
+          <option value="">По цене (дороже)</option>
+        </select>
+
+        <div class="flex relative">
+          <img
+            src="/search.svg"
+            alt=""
+            class="absolute top-1/2 left-3 -translate-y-1/2 cursor-pointer"
+          />
+
+          <input
+            type="text"
+            class="border border-slate-300 rounded-xl outline-none py-2 pl-10 pr-4 duration-300 focus:border-gray-400"
+            placeholder="Поиск..."
+          />
+        </div>
+      </div>
+    </div>
     <CardList />
   </div>
 </template>
--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,7 +1,39 @@
-<template>
-  <div class="fixed inset-0 bg-black opacity-60 z-10"></div>
+<script setup>
+  import DrawerHead from "./DrawerHead.vue";
+  import CartItemList from "./CartItemList.vue";
+</script>
 
-  <div class="bg-white w-96 right-0 top-0 bottom-0 fixed z-10">
+<template>
+  <div class="fixed inset-0 bg-black opacity-60 z-10">
     https://youtu.be/U_-Ht_v-oAs?t=8321
+  </div>
+
+  <div
+    class="bg-white w-96 right-0 top-0 bottom-0 fixed z-10 p-8 flex flex-col gap-6"
+  >
+    <DrawerHead />
+
+    <CartItemList />
+
+    <div class="flex flex-col gap-3 mt-auto">
+      <div class="flex gap-1">
+        <span>Итого:</span>
+        <div class="flex-1 border-b border-dotted border-gray-700"></div>
+        <b>12000 руб.</b>
+      </div>
+
+      <div class="flex gap-1">
+        <span>Налог 5%:</span>
+        <div class="flex-1 border-b border-dotted border-gray-700"></div>
+        <b>900 руб.</b>
+      </div>
+
+      <button
+        disabled
+        class="bg-lime-500 p-3 w-full text-white rounded-xl duration-300 hover:bg-lime-600 active:bg-lime-700 disabled:bg-slate-300 cursor-pointer mt-5"
+      >
+        Оформить заказ
+      </button>
+    </div>
   </div>
 </template>
